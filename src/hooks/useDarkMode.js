81de6a8974c6cc0829f16de77f295be653b70b4b
JGPico <minq_2@hotmail.com>
2020-03-11 14:53:44 -0600

    Met MVP

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,14 +1,18 @@
-import useLocalStorage from "./useLocalStorage";
+import { useLocalStorage } from "./useLocalStorage";
 import { useEffect } from "react";
 
-const useDarkMode = () => {
-    const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
+export const useDarkMode = () => {
+    const [changeMode, setChangeMode] = useLocalStorage('isDark');
     
     useEffect(() => {
-        if (darkMode) {
-            console.log("Hmm");
+        const body = document.querySelector('body');
+        if (changeMode) {
+            // add dark-mode class to body element
+            body.classList.add('dark-mode');
+        } else {
+            // remove dark-mode class from body element
+            body.classList.remove('dark-mode');
         }
-    },[])
+    },[changeMode])
+    return [changeMode, setChangeMode];
 }
-
-export default useDarkMode;
